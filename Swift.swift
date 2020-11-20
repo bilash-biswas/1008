043e6a519cf952fcc9a9a0ceaa30d4bf6a2340a9
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-21 00:36:18 +0600

    Create Swift.swift

---
+++ Swift.swift
@@ -0,0 +1,7 @@
+import Foundation
+var num1 = Int(readLine()!)!
+var num2 = Int(readLine()!)!
+var num3 = Double(readLine()!)!
+
+print("NUMBER = \(num1)")
+print(String(format: "SALARY = U$ %0.2f",(Double(num2) * num3)))
